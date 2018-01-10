37c35193a4b0b2601e90158fcf45f87790727070
Scott <scotthuston1982@gmail.com>
2018-01-10 17:17:26 -0500

    final commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,12 @@ Rails.application.routes.draw do
   get 'pictures/new' => 'pictures#new'
 
   get 'pictures/:id' => 'pictures#show'
+
+  root 'pictures#index'
+  get 'pictures' => 'pictures#index'
+
+  get 'pictures/:id/edit' => "pictures#edit"
+  patch 'pictures/:id' => "pictures#update"
+
+    delete 'pictures/:id' => 'pictures#destroy'
 end
